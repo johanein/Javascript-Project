e2df52afead1c94798c471c38eb9a7ab7bbf9ae7
Albert <johanein@gmail.com>
2020-04-26 23:39:58 +0530

    1st commit

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+import {
+    pElement,
+    h1Element,
+    buttonElement
+} from './domLoader'
+
+let buttonClicked = false
+
+buttonElement.addEventListener("click",() => {
+    buttonClicked = !buttonClicked     
+    if(buttonClicked){
+        pElement.style.display = "block"
+        pElement.style.color = "red"
+    }else{
+        pElement.style.color = "black"
+        pElement.style.display = "none"
+    }
+}
+)
+
+
---
+++ src/js/domLoader.js
@@ -0,0 +1,9 @@
+const pElement = document.querySelector('p')
+const h1Element = document.querySelector('h1')
+const buttonElement = document.querySelector('button')
+
+export {
+    pElement,
+    h1Element,
+    buttonElement
+}

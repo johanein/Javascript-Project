b3468ae13b79f7cef9be19ba0152e9e2e7f8f2ec
Albert <johanein@gmail.com>
2021-08-27 16:08:26 +0530

    improving apply in js

--- src/QoukkaFiles/JavascriptCardio/callApplyBindFunctions.js
+++ src/QoukkaFiles/JavascriptCardio/callApplyBindFunctions.js
@@ -7,15 +7,15 @@ const arr = [1, 2, 3];
 function funcToBeExecUsingCall(params) {
   return this.name + params;
 }
-function funcToBeExecUsingApply(params) {
-  return this.name + params;
+function funcToBeExecUsingApply(params, param2) {
+  return this.name + params + param2;
 }
 function funcToBeExecUsingBind(params) {
   return this.name + params;
 }
 
 console.log(funcToBeExecUsingCall.call(obj, 4)); // expect 9
-console.log(funcToBeExecUsingApply.apply(obj, arr)); // expect 6
+console.log(funcToBeExecUsingApply.apply(obj, arr)); // expect 8
 
 // binding the object to the function
 const bound = funcToBeExecUsingBind.bind(obj);
